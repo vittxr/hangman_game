d6e156ed23099a4127917c3241bca5cee6fba9ba
vittxr <vitor.roberto3022@gmail.com>
2024-04-11 22:35:57 -0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import json
 import random
 from typing import TypedDict
+from hangman_sprites import hangman_sprites
 
 
 class Word(TypedDict):
@@ -11,33 +13,46 @@ class Word(TypedDict):
 class HangmanGame:
     def __init__(self) -> None:
         self.__word: Word = self.__get_random_word()
+        self.theme: str = self.__word.get("theme")
         self.word_length = len(self.__word.get("name"))
+        self.masked_word = ["_"] * self.word_length
         self.guesses: int = 0
-        self.max_guesses: int = 6
+        self.max_guesses: int = 7
         self.guessed_letters: list[str] = []
         self.letters: list[str] = []
         self.game_over: bool = False
+        self.is_player_victorious: bool = False
+        self.endgame_message: None | str = None
 
     def __get_random_word(self) -> Word:
-        with open("words_db.json", mode="r") as file:
+        with open("./db/words.json", mode="r") as file:
             words = json.load(file)
         return random.choice(words)
 
     def __finish_game(self, is_winner: bool) -> None:
-        pass
+        self.is_player_victorious = True
+        self.game_over = True
+        if is_winner:
+            self.endgame_message = "Parabéns! Você acertou a palavra"
+        else:
+            self.endgame_message = (
+                f"Infelizmente você errou. A palavra era {self.__word.get('name')}"
+            )
 
     # TODO: add decorator to check if letter is string.
     def guess_letter(self, letter: str) -> None:
         self.letters.append(letter)
 
-        if letter in self.__word:
+        if letter in self.__word.get("name"):
+            letter_idx = self.__word.get("name").index(letter)
+            self.masked_word[letter_idx] = letter
+
             self.guessed_letters.append(letter)
             if self.word_length == len(self.guessed_letters):
                 self.__finish_game(is_winner=True)
         else:
             self.guesses += 1
             if self.guesses == self.max_guesses:
-                self.game_over = True
                 self.__finish_game(is_winner=False)
 
     def guess_word(self, word: str) -> None:
@@ -46,8 +61,64 @@ class HangmanGame:
             self.__finish_game(is_winner=True)
 
 
-class GameUI(HangmanGame):
-    def __init__(
-        self,
-    ):
-        super().__init__()
+# TODO: All text should be stored in a file called text.py
+class GameUI:
+    def __init__(self):
+        self.start()
+
+    def start(self) -> None:
+        game = HangmanGame()
+        with open("./db/players_scores.json", mode="r") as file:
+            players_scores = json.load(file)
+
+        player_name = input("Digite o nome do jogador: ")
+
+        while not game.game_over:
+            print(f"TEMA - {game.theme}")
+            print(hangman_sprites[game.guesses])
+
+            print(" ".join(game.masked_word))
+
+            print("[1] - Adivinhar letra")
+            print("[2] - Adivinhar palavra")
+            option = input()
+
+            match option:
+                case "1":
+                    print("Digite a letra: ")
+                    char = input()
+                    game.guess_letter(char)
+                case "2":
+                    print("Digite a palavra")
+                    word = input()
+                    game.guess_word(word)
+                case _:
+                    print("Opção inválida!")
+
+        if game.is_player_victorious:
+            win_count = 1
+            if players_scores.get(player_name):
+                win_count = players_scores[player_name]["win_count"] + 1
+
+            players_scores[player_name] = {"win_count": win_count}
+        else:
+            loses_count = 1
+            if players_scores.get(player_name):
+                win_count = players_scores[player_name]["loses_count"] + 1
+
+            players_scores[player_name] = {"loses_count": loses_count}
+
+        os.system("cls")
+        print(f"JOGO FINALIZADO, {player_name} \n")
+        print(game.endgame_message)
+        print("Status do jogados: ")
+        print("Número de vitórias: ", players_scores.get(player_name).get("win_count"))
+        print(
+            "Número de derrotas: ", players_scores.get(player_name).get("loses_count")
+        )
+
+        with open("./db/players_scores") as f:
+            f.write(players_scores)
+
+
+GameUI()
